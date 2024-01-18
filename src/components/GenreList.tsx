89e44f813ca54d4f35132074b574d026b20dff6c
Mansoor Hussain <qaisrani07@gmail.com>
2024-01-18 12:27:16 +0500

    Show skeleton while fetching genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,11 +1,24 @@
-import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import {
+  HStack,
+  Image,
+  List,
+  ListItem,
+  SkeletonText,
+  Text,
+} from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
-  const { data: genres } = useGenres();
+  const { data: genres, isLoading } = useGenres();
+  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8];
+
   return (
     <List>
+      {isLoading &&
+        skeletons.map((skeleton) => (
+          <SkeletonText key={skeleton} height={20} />
+        ))}
       {genres.map((genre) => (
         <ListItem key={genre.id} paddingY={1.5}>
           <HStack>
--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -32,11 +32,7 @@ const PlatformIconList = ({ platforms }: Props) => {
   return (
     <HStack marginY={1}>
       {platforms.map((platform) => (
-        <Icon
-          id={platform.id.toString()}
-          as={iconMap[platform.slug]}
-          color="gray.500"
-        />
+        <Icon key={platform.id} as={iconMap[platform.slug]} color="gray.500" />
       ))}
     </HStack>
   );
