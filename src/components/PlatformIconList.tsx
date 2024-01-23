97397aa2ac5516587eec1fab653555e1053594a2
Mansoor Hussain <qaisrani07@gmail.com>
2024-01-23 20:14:30 +0500

    Refactor: Move entity interfaces

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -11,7 +11,7 @@ import { SiNintendo } from "react-icons/si";
 import { IconType } from "react-icons";
 import { BsGlobe } from "react-icons/bs";
 import { HStack, Icon } from "@chakra-ui/react";
-import { Platform } from "../hooks/usePlatforms";
+import { Platform } from "../entities/Platform";
 
 interface Props {
   platforms: Platform[];
--- src/hooks/useGame.ts
+++ src/hooks/useGame.ts
@@ -1,6 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import APIClient from "../services/apiCient";
-import { Game } from "./useGames";
+import { Game } from "../entities/Game";
 
 const apiCient = new APIClient<Game>("/games");
 
